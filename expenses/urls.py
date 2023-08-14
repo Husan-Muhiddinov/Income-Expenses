5b0bf6cb971196c61dbf485f43996d3296960c07
Husan-Muhiddinov <khusanmukhiddinov43@gmail.com>
2023-08-14 17:57:00 +0500

    Expenses

---
+++ expenses/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import index, add_expense
+
+urlpatterns = [
+    path('', index, name='expenses'),
+    path('add-expense', add_expense, name='add-expense'),
+]
